294f44340916c8f96f846f8fa5804ef67dd8af29
mavdotj <ceo@marinadev.xyz>
2025-02-15 19:55:52 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,5 +9,9 @@
 		<a class="btn" href='/'>Home</a>
 		<a class="btn" href='/blog'>Blog</a>
 		<a class="btn" href='/contact'>Contact</a>
+
+  <a href="https://bsky.app/profile/kaidoesthings.xyz" rel="me">@kaidoesthings.xyz on Blueksy</a>
+  <a href="https://github.com/mavdotj" rel="me">Github</a>
+  <a href="mailto:kai@kaidoesthings.xyz" rel="me">kai@kaidoesthings.xyz</a>
 </div>
 {@render children()}
